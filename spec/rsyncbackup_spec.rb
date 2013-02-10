f182d94e41f67750bca0d2711a40d3175515d15b
Tamara Temple <tamouse@gmail.com>
2013-02-10 03:40:19 -0600

    add source and target for Interface Checks

--- spec/rsyncbackup_spec.rb
+++ spec/rsyncbackup_spec.rb
@@ -5,7 +5,7 @@ describe Rsyncbackup do
   
   context "Interface Checks" do
     it { Rsyncbackup.should respond_to(:new) }
-    it { Rsyncbackup.new().should be_a(Rsyncbackup) }
+    it { Rsyncbackup.new(source: 'source', target: 'target').should be_a(Rsyncbackup) }
   end
   
   context "Options Validity" do
