3d24089233cc1c3209aec445a6abf0bfbc8cbf82
Tamara Temple <tamouse@gmail.com>
2013-02-10 03:36:21 -0600

    set :link_dest after setting :source and :target

--- lib/rsyncbackup.rb
+++ lib/rsyncbackup.rb
@@ -1,5 +1,6 @@
 require "rsyncbackup/version"
 require "rsyncbackup/utilities"
+require 'open3'
 require 'methadone'
 
 class Rsyncbackup
@@ -21,6 +22,37 @@ class Rsyncbackup
       rsync_cmd: rsync_executable
     }.merge(opts)
     
+    options[:source] = strip_trailing_separator_if_any(options[:source])
+    options[:target] = strip_trailing_separator_if_any(options[:target])
+
+    options[:link_dest] ||= last_full_backup
+
+
     debug "options: #{options.inspect}"
+  end
+
+
+  def run
+    cmd = build_command
+    
+    info "Rsync command: #{cmd}"
+    if options[:dry_run]
+      info "Dry run only"
+    else
+      
+      Open3.popen3(cmd) do |stdin, stdout, stderr, wait|
+        stdin.close
+        until stdout.eof?
+          info stdout.gets
+        end
+        until stderr.eof?
+          errors =  stderr.gets
+        end
+        result = wait.value
+        raise "Command failed. Return code: #{result}\n#{errors}" unless result == 0
+      end
+    
+    end
+    
   end
 end
